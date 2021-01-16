652aa3409a581f60461c6ea3a06183c05537bb38
Harshit Sharma <harshitsharmabtp@gmail.com>
2021-01-16 17:53:50 +0530

    create react app initialisation

---
+++ blogapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './Components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
